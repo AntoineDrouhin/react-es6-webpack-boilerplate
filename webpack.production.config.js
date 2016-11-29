4bf1dd47b2d830a46385182248ca093f0e8ec8a4
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-11-29 11:07:58 +0100

    webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry:
+    './source/index.js'
+  ,
+  output: {
+    filename: 'bundle.js',
+    path:  './dist',
+    publicpath: './dist'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, loader: "style!css" }
+    ],
+    plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  }};
